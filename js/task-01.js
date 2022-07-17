b9a06d5f8a578009dad61941d0df548577338f45
Roman_Onishchuk <romanonisuk@gmail.com>
2022-07-18 00:24:12 +0300

    Update task 01

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const itemsEl = document.querySelectorAll(".item");
+console.log(`Number of categories: ${itemsEl.length}`);
+
+const categoryTitles = document.querySelector("h2");
+
+console.log(categoryTitles.textContent);
+
