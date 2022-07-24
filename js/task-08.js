802d5c1a42137f9411b680e12ca6848fe9e466f1
Roman_Onishchuk <romanonisuk@gmail.com>
2022-07-25 01:02:31 +0300

    task-8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(e) {
+
+    e.preventDefault();
+    e.currentTarget.elements.email.value === '' || e.currentTarget.elements.password.value === '' ? alert("Все поля должны быть заполнены!") : "";
+    const formData = {
+        email: e.currentTarget.elements.email.value,
+        password: e.currentTarget.elements.password.value,
+    };
+
+    console.log(formData);
+
+    e.currentTarget.reset();
+};
 
