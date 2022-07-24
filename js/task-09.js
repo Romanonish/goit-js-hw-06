10b56008398a9adee4efac3dd20996c4c17a5628
Roman_Onishchuk <romanonisuk@gmail.com>
2022-07-25 01:21:39 +0300

    task-9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  buttonEl: document.querySelector(".change-color"), 
+  colorValEl: document.querySelector(".color"),
+  bodyEl: document.querySelector("body"),
+};
+
+refs.buttonEl.addEventListener('click', () => {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  refs.colorValEl.textContent = refs.bodyEl.style.backgroundColor;
+});
