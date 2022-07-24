1e9204b7fd8d408738b956f99f98c70763072dc8
Roman_Onishchuk <romanonisuk@gmail.com>
2022-07-25 02:26:36 +0300

    task-10 in-progress

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,22 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  formEl: document.querySelector('input'),
+  btnCreate: document.querySelector('button[data-create]'),
+  btnDestroy: document.querySelector('button[data-destroy]'),
+  insBoxEl: document.querySelector('#boxes'),
+
+}
+
+function createBoxes() { 
+  document.createElement(`div`);
+refs.insBoxEl.append()
+};
+
+refs.btnCreate.addEventListener('click', createBoxes(refs.formEl.value));
+
+console.log("~ refs.formEl", refs.formEl.value)
+console.log("~ refs.formEl", refs.btnCreate)
+console.log("~ refs.formEl", refs.btnDestroy)
